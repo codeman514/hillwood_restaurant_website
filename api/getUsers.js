47151d3423e8f313c7794c9af49dcde509aaeb99
codeman514 <gmail>
2026-04-22 14:07:48 +0800

    +

--- api/getUsers.js
+++ api/getUsers.js
@@ -1,5 +1,5 @@
 import { neon } from '@neondatabase/serverless';
-export default async function handler(req,res)
+export default function handler(req,res)
 {
     //const sql = neon(process.env.DATABASE_URL);
     res.status(200).json({message:"Hello World"});
