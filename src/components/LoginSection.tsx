40fdd904b5134b78e45d6fb8bc2ed0b1c0a8d103
codeman514 <gmail>
2026-04-22 13:09:15 +0800

    +

--- src/components/LoginSection.tsx
+++ src/components/LoginSection.tsx
@@ -1,26 +1,44 @@
 import axios from "axios";
-import {useState} from "react";
+import { useState } from "react";
 export default function LoginSection() {
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
-  const [content,setContent] = useState("無");
-  const handleSubmit = async (event:React.SubmitEvent) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [content, setContent] = useState("無");
+  const handleSubmit = async (event: React.SubmitEvent) => {
     event.preventDefault();
-    if(!username || !password)
-    {
-      console.log("欠缺帳戶或密碼");
+    if (!username || !password) {
+      alert("請輸入帳戶和密碼");
     }
-    const response = await axios.post("/api/getUser",{
-      username:username,
-      password:password
-    });
-    setContent(response.data.message);
+    // const response = await axios.post("/api/getUser",{
+    //   username:username,
+    //   password:password
+    // });
+    fetch("/api/getUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ username: username, password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    // setContent(response.data.message);
   };
   return (
     <section className="d-flex justify-content-center align-items-center custom-login-section">
       <form className="col-6 h-50" onSubmit={handleSubmit}>
-        <input type="text" placeholder="輸入帳戶" value = {username} onChange={event=>setUsername(event.target.value)}/>
-        <input type="password" placeholder="輸入密碼" value = {password} onChange={event=>setPassword(event.target.value)}/>
+        <input
+          type="text"
+          placeholder="輸入帳戶"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="輸入密碼"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <button type="submit">登入</button>
         <div>{content}</div>
       </form>
